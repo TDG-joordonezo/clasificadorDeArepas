ef3bde96d513cbd8712b4d1e8de1eac364bf6da4
joordonezo <jorgeluisordonez1995@gmail.com>
2023-11-06 01:00:02 -0500

    refactor model code and add validation code

--- modelTensorFlow.py
+++ modelTensorFlow.py
@@ -2,89 +2,112 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
+import tensorflowjs as tfjs
 
 print("Versión de TensorFlow:", tf.__version__)
 
-def to_grayscale_from_rgb(image):
-  image = tf.convert_to_tensor(image)  
-  return image
 
-#imprimir primera imagen
-image = tf.io.read_file('./arepasDatasetSinFondo/defectuosas/IMG_20230820_171451984.png')
+def to_grayscale_from_rgb(image):
+    image = tf.convert_to_tensor(image)
+    return image
+
+
+# imprimir primera imagen
+image = tf.io.read_file(
+    './arepasDatasetMejoradas\entrenamiento\defectuosas\IMG_20231104_002007554.jpg')
 image = tf.image.decode_jpeg(image, channels=3)
 gray_image = to_grayscale_from_rgb(image)
 print(gray_image.shape)
 plt.imshow(gray_image, cmap='gray')
 plt.show()
 
-TAM_X_ORIG=3000
-TAM_Y_ORIG=4000
-X_TAM=int(TAM_X_ORIG * .08)
-Y_TAM=int(TAM_Y_ORIG * .08)
-BATCH_SIZE=32
+TAM_X_ORIG = 240
+TAM_Y_ORIG = 320
+X_TAM = int(TAM_X_ORIG * 1)
+Y_TAM = int(TAM_Y_ORIG * 1)
+BATCH_SIZE = 32
+tf.random.set_seed(1)
 
-datos = keras.preprocessing.image.ImageDataGenerator(
-    rescale=1. /255,
-    validation_split=0.2,
-    width_shift_range=0.05,
-    height_shift_range=0.05,
+train_data_generator = keras.preprocessing.image.ImageDataGenerator(
+    rescale=1. / 255,
+    validation_split=0.1,
+    width_shift_range=0.01,
+    height_shift_range=0.03,
     horizontal_flip=True,
     vertical_flip=True,
-    rotation_range=90,
-    zoom_range=[.5,1.05],
-    shear_range=5,
+    rotation_range=.01,
     preprocessing_function=to_grayscale_from_rgb
 )
 
-path_data="./arepasDatasetSinFondo"
-data_entrenamiento = datos.flow_from_directory(path_data,
-                                               target_size=(X_TAM, Y_TAM),
-                                               color_mode="grayscale",
-                                               batch_size=BATCH_SIZE,
-                                               shuffle=True,
-                                               class_mode='categorical',
-                                               subset="training"
-                                               )
+validation_data_generator = keras.preprocessing.image.ImageDataGenerator(
+    rescale=1. / 255,
+    validation_split=0.1,
+)
 
-data_validacion = datos.flow_from_directory(path_data,
-                                               target_size=(X_TAM, Y_TAM),
-                                               color_mode="grayscale",
-                                               batch_size=BATCH_SIZE,
-                                               shuffle=True,
-                                               class_mode='categorical',
-                                               subset="validation"
-                                               )
+path_data = "./arepasDatasetMejoradas"
+path_entrenamiento = path_data + "/entrenamiento"
+path_pruebas = path_data + "/pruebas"
+data_entrenamiento = train_data_generator.flow_from_directory(path_entrenamiento,
+                                                              target_size=(
+                                                                  X_TAM, Y_TAM),
+                                                              color_mode="grayscale",
+                                                              batch_size=BATCH_SIZE,
+                                                              shuffle=True,
+                                                              class_mode='categorical',
+                                                              subset="training"
+                                                              )
 
+data_validacion = validation_data_generator.flow_from_directory(path_entrenamiento,
+                                                                target_size=(
+                                                                    X_TAM, Y_TAM),
+                                                                color_mode="grayscale",
+                                                                batch_size=BATCH_SIZE,
+                                                                shuffle=True,
+                                                                class_mode='categorical',
+                                                                subset="validation"
+                                                                )
 
-#ver imagen de prueba en plt
-plt.figure(figsize=(10,10))
-for i, (imagenes, etiquetas) in enumerate(data_entrenamiento):
-    print("Shape: ",imagenes[0].shape)
-    if i <= 25:
-        plt.subplot(5,5,i+1)
+data_test = validation_data_generator.flow_from_directory(path_pruebas,
+                                                          target_size=(
+                                                              X_TAM, Y_TAM),
+                                                          color_mode="grayscale",
+                                                          batch_size=BATCH_SIZE,
+                                                          shuffle=False,
+                                                          class_mode='categorical',
+                                                          )
+
+# ver imagen de prueba en plt
+plt.figure(figsize=(24, 18))
+count = 0
+for imagenes, etiquetas in data_entrenamiento:
+    print("Shape: ", imagenes[0].shape)
+    for i in range(len(imagenes)):
+        plt.subplot(5, 5, count + 1)
         plt.xticks([])
         plt.yticks([])
         plt.imshow(imagenes[i], cmap='gray')
+        count += 1
+        if count >= 25:
+            break
+    if count >= 25:
         break
-    else:
-       break  
+
 plt.show()
-    
+
 print(data_entrenamiento.class_indices)
 
 modelo = keras.Sequential([
-    keras.layers.Conv2D(32, (3,3), input_shape=(X_TAM,Y_TAM,1), activation="relu"),
-    keras.layers.MaxPooling2D(2,2),
+    keras.layers.Conv2D(32, (3, 3), input_shape=(X_TAM, Y_TAM, 1), activation="relu"),
+    keras.layers.MaxPooling2D(2, 2),
 
-    keras.layers.Conv2D(64, (3,3), activation="relu"),
-    keras.layers.MaxPooling2D(2,2),
+    keras.layers.Conv2D(64, (3, 3), activation="relu"),
+    keras.layers.MaxPooling2D(2, 2),
 
-    keras.layers.Conv2D(128, (3,3), activation="relu"),
-    keras.layers.MaxPooling2D(2,2),
+    keras.layers.Conv2D(128, (3, 3), activation="relu"),
+    keras.layers.MaxPooling2D(2, 2),
 
     keras.layers.Flatten(),
     keras.layers.Dense(250, activation="relu"),
-    #keras.layers.Dropout(0.5),
     keras.layers.Dense(2, activation="softmax"),
 ])
 
@@ -93,9 +116,9 @@ modelo.compile(
     loss="categorical_crossentropy",
     metrics=["accuracy"]
 )
-
-tensorboardCNN = keras.callbacks.TensorBoard(log_dir='logs/cnnNoDout250')
-EPOCAS = 50
+EPOCAS = 100
+modelName = '3Xfilter+D250+E'+str(EPOCAS)+'+S1'
+tensorboardCNN = keras.callbacks.TensorBoard(log_dir=('logs/'+modelName))
 entrenamiento = modelo.fit(
     data_entrenamiento,
     batch_size=BATCH_SIZE,
@@ -105,3 +128,13 @@ entrenamiento = modelo.fit(
     validation_steps=int(np.ceil(data_validacion.n / float(BATCH_SIZE))),
     callbacks=[tensorboardCNN]
 )
+print("Guardando model .h5")
+modelo.save(('./modelos/'+modelName+'/keras/modelo.h5'))
+score = modelo.evaluate(data_test)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
+
+# guardar modelo en tensorflowjs
+print("Guardando model tfjs")
+tfjs.converters.save_keras_model(modelo, ('./modelos/'+modelName+'/tfjs'))
+print("Finalizó todo correctamente!!!")
---
+++ validateModel.py
@@ -0,0 +1,52 @@
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+TAM_X_ORIG = 240
+TAM_Y_ORIG = 320
+X_TAM = int(TAM_X_ORIG * 1)
+Y_TAM = int(TAM_Y_ORIG * 1)
+BATCH_SIZE = 32
+
+
+validation_data_generator = keras.preprocessing.image.ImageDataGenerator(
+    rescale=1. / 255,
+    validation_split=0.1,
+)
+path_data = "./arepasDatasetMejoradas"
+path_pruebas = path_data + "/pruebasNo"
+
+data_test = validation_data_generator.flow_from_directory(path_pruebas,
+                                                          target_size=(
+                                                              X_TAM, Y_TAM),
+                                                          color_mode="grayscale",
+                                                          batch_size=BATCH_SIZE,
+                                                          shuffle=False,
+                                                          class_mode='categorical',
+                                                          )
+
+modelName= 'Cruzado3-dataAument-split0-1+3Xfilter+D250+E100+S1'
+loaded_model = keras.models.load_model('./modelos/' + modelName + '/keras/modelo.h5')
+
+test_score = loaded_model.evaluate(data_test)
+print("Test loss:", test_score[0])
+print("Test accuracy:", test_score[1])
+
+predictions = loaded_model.predict(data_test)
+images, labels = next(data_test)
+predicted_labels = np.argmax(predictions, axis=1)
+class_names = ["Perfecta", "Defectuosa"]
+
+def plot_images(images, labels, predicted_labels, class_names):
+    plt.figure(figsize=(12, 12))
+    for i in range(min(len(images), 9)):  # Mostrar hasta 9 imágenes
+        plt.subplot(3, 3, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(images[i][0], cmap='gray')
+        actual_label = np.argmax(labels[i])
+        predicted_label = predicted_labels[i]
+        plt.xlabel(f"Real: {class_names[actual_label]}\nPredicción: {class_names[predicted_label]}")
+    plt.show()
+
+plot_images(images, labels, predicted_labels, class_names)
